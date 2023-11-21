ee77681ffd530bb2e3ca10e8a7e35d7a366aa331
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-11-21 15:05:45 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int n,s=0,c=0;     cin>>n;     for(int i=1;i<=n;i++)     {         s=s+(i*i);         c=c+i;     }     int sum=c*c;     int d=abs(sum-s);     cout<<d;
    }

---
+++ Difference_of_Sums.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,s=0,c=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        s=s+(i*i);
+        c=c+i;
+    }
+    int sum=c*c;
+    int d=abs(sum-s);
+    cout<<d;
+    
+}
